a3003af2b31363f82ea1bd253ab0f5fb83d5274b
Florian-BRUNEAU <contact@fbruneau.fr>
2016-03-11 17:10:56 +0100

    Structuration

---
+++ README.md
@@ -0,0 +1,11 @@
+# Robot-Atmel / sources
+
+###Équipe 2
+Alibert Jerôme - Bardet Guillaume - Bogi Vincent - Bouder Clément - Bourniquel Florian - Bruneau Florian
+
+###Basecamp
+https://basecamp.com/2577854/projects/11731326
+
+###Infos utiles
+- Robot Atmel
+- 3 branches :  master <-- dev <-- features
---
+++ doc/arduino-1.6.7/portable/sketchbook/libraries/Atmel_University/examples/E1-blinky/E1-blinky.ino
@@ -0,0 +1,47 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the Uno and
+  Leonardo, it is attached to digital pin 13. If you're unsure what
+  pin the on-board LED is connected to on your Arduino model, check
+  the documentation at http://www.arduino.cc
+
+  This example code is in the public domain.
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+
+ modified jan 2016 , Bob Martin Atmel Corp
+
+
+ */
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+  // initialize digital pin 13 as an output.
+  pinMode(PIN_LED_13, OUTPUT);
+
+  Serial.begin(115200);
+  while(!Serial) ;
+
+
+}
+
+// the loop function runs over and over again forever
+// infamous double blip
+
+void loop()
+{
+  Serial.println("blink");
+
+  digitalWrite(PIN_LED_13, HIGH);
+  delay(100);
+  digitalWrite(PIN_LED_13, LOW);
+  delay(100);
+  digitalWrite(PIN_LED_13, HIGH);
+  delay(100);
+  digitalWrite(PIN_LED_13, LOW);
+  delay(700);
+}
