6e1804ab6fea7b09aadfdae19ffbfafc3a883f83
Florian Bourniquel <florianbourniquel@gmail.com>
2016-03-25 10:41:07 +0100

    Renommage du dossier et des ficher de la mission 1 et ajout du fichier de Test. Fait avec Clement Bouder

---
+++ DetectionMur/DetectionMur.h
@@ -0,0 +1,13 @@
+test(Pres)
+{
+  int x=660;
+  assertMoreOrEqual(recupererDistanceCapteur(0),x);
+}
+
+test(Colision)
+{
+  bool x=true;
+  assertEqual(x,isColision(0));
+}
+
+
---
+++ DetectionMur/DetectionMur.ino
@@ -0,0 +1,30 @@
+#include <ArduinoUnit.h>
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  while (true) {
+    Test::run();
+  }
+}
+
+int recupererDistanceCapteur(int numeroPin) {
+  int val = analogRead(numeroPin);
+  Serial.println(val);
+  return val;
+}
+
+bool isColision(int numeroPin) {
+  if(recupererDistanceCapteur(numeroPin) > 660) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+#include "DectionMur.h"
