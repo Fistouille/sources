fb64d02882e02d2507e75798a1a7db515e1b50fd
Jérôme Alibert <jeromealibert@hotmail.fr>
2016-03-25 16:36:29 +0100

    Ajout du bouton d'arret d'urgence (Blink)

--- DetectionMur/DetectionMur.ino
+++ DetectionMur/DetectionMur.ino
@@ -1,20 +1,43 @@
 #include <ArduinoUnit.h>
 #include <Servo.h>
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <SPI.h>
+#include <WiFi101.h>
+#include <BlynkSimpleWiFiShield101.h>
+#include <Servo.h>
 
-Servo myServo;
-Servo myServo2;
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "2603b8b0a9ac4be5a9b8c8f04ceffecb";
+// Your WiFi credentials
+char ssid[] = "Wifi_Arduino";
+char pass[] = "aarduino";
+
+BLYNK_WRITE(V2)
+{
+  if(param.asInt()== 1){
+    servo2.write(90);
+    servo.write(90);
+  }
+  else{
+    servo.write(53);
+    servo2.write(130);
+  }
+}
 
 void setup()
 {
   myServo.attach(12);
   myServo2.attach(13);
   myServo.write(130);
-  myServo.write(53);  
+  myServo.write(53);
+  Blynk.begin(auth, ssid, pass);
   Serial.begin(9600);
 }
 
 void loop()
 {
+  Blynk.run();
   if(isColision(&recupererDistanceCapteur, 0){
     myServo.write(90);
     myServo2.write(90);
