1fe9cc6fa95a2dbcc7d809f830e28c242130210b
unknown <b14007185@A-F002076.salsa.univ-amu.fr>
2016-03-30 16:40:53 +0200

    Modification de la fonction de détection d'angle

--- Boussole/Boussole.ino
+++ Boussole/Boussole.ino
@@ -1,66 +1,29 @@
-#include <Adafruit_BNO055.h>
-#include <Adafruit_Sensor.h>
-
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
-
-#define BNO055_SAMPLERATE_DELAY_MS (100)
-
+  
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
-
-void displaySensorDetails(void)
+ 
+void setup(void) 
 {
-  sensor_t sensor;
-  bno.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-}
-
-void setup(void)
-{
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("Orientation Sensor Test"); Serial.println("");
-
   if(!bno.begin())
   {
-
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
 
-  delay(500);
-  displaySensorDetails();
+  delay(1000);
+  bno.setExtCrystalUse(true);
 }
-
-void loop(void)
+ 
+void loop(void) 
 {
-  sensors_event_t event;
+  sensors_event_t event; 
   bno.getEvent(&event);
-
-  Serial.print(F("Orientation: "));
-  Serial.print((float)event.orientation.x); // Fournit un angle de 0 à 360°
-
-  uint8_t sys, gyro, accel, mag = 0;
-  bno.getCalibration(&sys, &gyro, &accel, &mag);
-  Serial.print(F("Calibration: "));
-  Serial.print(sys, DEC);
-  Serial.print(F(" "));
-  Serial.print(gyro, DEC);
-  Serial.print(F(" "));
-  Serial.print(accel, DEC);
-  Serial.print(F(" "));
-  Serial.println(mag, DEC);
-
+  Serial.print("X: ");
+  Serial.println(event.orientation.x, 4);
   delay(100);
 }
-
