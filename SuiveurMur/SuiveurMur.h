de6889c9d9d2f344281482cf470e1b34e8adb70f
Jérôme Alibert <jeromealibert@hotmail.fr>
2016-03-30 15:41:52 +0200

    Ajout des tests unitaires et simulation du comportement des capteur (Avec Florian Bourniquel)

---
+++ SuiveurMur/SuiveurMur.h
@@ -0,0 +1,16 @@
+int retourSansObstacle(int pin){
+  return 700
+}
+
+int retourObstacle(int pin){
+  return 500
+}
+
+int bonneDistance(int pin){
+  return 650
+}
+
+int mauvaiseDistance(int pin){
+  return 
+}
+
