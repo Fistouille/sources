ed937eec928b82261399e291d51582ad17cd7088
GuillaumeB <bguillaume81@gmail.com>
2016-03-30 14:39:58 +0200

    modification et ajustement du sens de rotation des Servomoteurs

--- SuiveurMur/SuiveurMur.ino
+++ SuiveurMur/SuiveurMur.ino
@@ -1,7 +1,14 @@
 #include <Servo.h>
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <SPI.h>
+#include <WiFi101.h>
+#include <BlynkSimpleWiFiShield101.h>
 
-Servo myservo;  // create servo object to control a servo
-Servo myservo2;  // create servo object to control a servo
+char auth[] = "2603b8b0a9ac4be5a9b8c8f04ceffecb";
+char ssid[] = "Wifi_Arduino";
+char pass[] = "aarduino"; 
+Servo myservoG;  // create servo object to control a servo
+Servo myservoD;  // create servo object to control a servo
 
 int sigAvant;
 int sigDroite;
@@ -9,25 +16,42 @@ int sigGauche;
 int sigArriere;
   
 void setup() {
+ // Blynk.begin(auth, ssid, pass);
   Serial.begin(115200);
-  myservo.attach(12);  // attaches the servo on pin 12 to the servo object
-  myservo2.attach(13);  // attaches the servo on pin 13 to the servo object
+  myservoG.attach(12);  // attaches the servo on pin 12 to the servo object
+  myservoD.attach(13);  // attaches the servo on pin 13 to the servo object
 }
 
+// Gauche de 0 a 90 pour avancer
+// Droite de 180 a 90 pour avancer
+/*
+BLYNK_WRITE(V2)
+{
+  if(param.asInt()== 1){
+    myservoD.write(90);
+    myservoG.write(90);
+  }
+  else{
+    myservoG.write(53);
+    myservoD.write(130);
+  }
+}
+*/
 void loop() {
+  //Blynk.run();
   sigAvant = analogRead(0); 
   sigGauche = analogRead(1);
 
   if(sigAvant < 660 &&/*rien devant et distance gauche compris entre xx et yy*/ sigGauche < 700 && sigGauche > 600)
     avancer();
   else if(sigAvant < 660 &&/* rien devant et distance gauche inférieure à xx*/ sigGauche <= 600)
-    gauche();
-  else if(sigAvant < 660 &&/* rien devant et distance gauche supérieure à yy*/ sigGauche >= 700)
     droite();
+  else if(sigAvant < 660 &&/* rien devant et distance gauche supérieure à yy*/ sigGauche >= 700)
+    gauche();
   else if(sigAvant >= 660)
-    droite();  
+    droiteObstacle();  
 
-  delay(300);
+  delay(100);
   
 }
 
@@ -35,31 +59,37 @@ void loop() {
 
 // avancer
 void avancer() {
-   myservo.write(78);    // vitesse en avancant                           
-   myservo2.write(110); 
+   myservoG.write(78);    // vitesse en avancant                           
+   myservoD.write(110); 
 }
 
 //reculer
 void reculer() {
-   myservo.write(109);    // vitesse en reculant                          
-   myservo2.write(78); 
+   myservoG.write(109);    // vitesse en reculant                          
+   myservoD.write(78); 
 }
 
 // tourner à droite
 void droite() {
-   myservo.write(90);    // vitesse tournant                           
-   myservo2.write(110); 
+   myservoG.write(75);    // vitesse tournant                           
+   myservoD.write(150); 
+}
+
+// tourner en cas d'obstacle
+void droiteObstable() {
+   myservoG.write(80);    // vitesse tournant                           
+   myservoD.write(90); 
 }
 
  // tourner à gauche
 void gauche() {
-   myservo.write(77);    // vitesse tournant
-   myservo2.write(90); 
+   myservoG.write(40);    // vitesse tournant
+   myservoD.write(105); 
 }
 
  // arret
 void arreter() {
-   myservo.write(90);    // vitesse tournant
-   myservo2.write(90); 
+   myservoG.write(90);    // vitesse tournant
+   myservoD.write(90); 
 }
 
